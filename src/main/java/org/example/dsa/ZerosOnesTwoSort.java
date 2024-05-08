8b5eb6e9579d1ab194c144f0624469fcb873add3
4812566 <vishesh.tiwari@lowes.com>
2024-05-08 16:00:25 +0530

    added 0s 1s 2s solution

---
+++ src/main/java/org/example/dsa/ZerosOnesTwoSort.java
@@ -0,0 +1,27 @@
+package org.example.dsa;
+
+import java.util.Arrays;
+
+public class ZerosOnesTwoSort {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+        Arrays.stream(sortedArray(arr)).forEach((x) -> System.out.print(x + " "));
+    }
+
+    private static int[] sortedArray(int[] arr) {
+        int[] map = new int[3];
+        for (int j : arr) {
+            ++map[j];
+        }
+
+        int[] res = new int[arr.length];
+        int k = 0;
+
+        for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i]; j++) {
+               res[k++] = i;
+            }
+        }
+        return res;
+    }
+}
