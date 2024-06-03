8ab3ea1225bf7e1e49fce7a9574ef9c0f05ad4f7
4812566 <vishesh.tiwari@lowes.com>
2024-06-03 12:22:53 +0530

    added alternate problem sol

---
+++ src/main/java/org/example/dsa/LongestConSeq.java
@@ -0,0 +1,27 @@
+package org.example.dsa;
+
+import java.util.Arrays;
+
+public class LongestConSeq {
+    public static void main(String[] args) {
+        int[] arr = {100, 200, 1, 3, 2, 4};
+        System.out.print(calculateLongestComSeq(arr));
+    }
+
+    private static int calculateLongestComSeq(int[] arr) {
+        Arrays.sort(arr);
+        int sizeOfSequence = 1;
+        int temp = arr[0];
+        int i = 0;
+
+        while (i<arr.length) {
+            if(temp + 1 == arr[i]) {
+                sizeOfSequence++;
+            }
+            temp = arr[i];
+            i++;
+        }
+
+        return sizeOfSequence;
+    }
+}
